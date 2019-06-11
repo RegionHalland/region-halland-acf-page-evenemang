d1e30bc63e26b2b1dd20e956c7ff4b69cfeeed7e
rhn014 <Roland.Hyden@RegionHalland.se>
2019-06-11 17:07:38 +0200

    Version 2.0.0 med puff, biljett och bild

--- README.md
+++ README.md
@@ -57,7 +57,7 @@ OBS! Justera så att du hämtar aktuell version.
 @php($myData = get_region_halland_acf_page_evenemang_kommande_items())
 @if(isset($myData))
   @foreach ($myData as $data)
-    {!! $data->image !!}<br>
+    <img src="{!! $data->puff_url !!}"<br>
     {{ $data->post_title }}<br>
     {{ $data->ingress }}<br>
   @endforeach
@@ -103,106 +103,14 @@ array (size=2)
       public 'speltid' => string '2019-06-29 18:30' (length=16)
       public 'speltid_datum' => string '2019-06-29' (length=10)
       public 'speltid_tid' => string '18:30' (length=5)
+      public 'puff_url' => string 'http://exempel.se/app/uploads/2019/06/hero-i-bygg.jpg' (length=53)
+      public 'puff_width' => int 1440
+      public 'puff_height' => int 420
+      public 'puff_has_url' => int 1
 ```
 
 
-## Visa alla kommande events på en sida via "Blade" (med all information)
-
-```sh
-@php($myData = get_region_halland_acf_page_evenemang_kommande_items_all())
-@if(isset($myData))
-  @foreach ($myData as $data)
-    {!! $data->image !!}<br>
-    {{ $data->post_title }}<br>
-    {{ $data->ingress }}<br>
-    Information<br>
-      @foreach ($data->information as $information)
-        @if($information['has_link'])
-          <a href="{{ $information['link_url'] }}" target="{{ $information['link_target'] }}">{{ $information['link_title'] }}</a><br>
-        @endif
-      @endforeach
-      Arrangörer<br>
-      @foreach ($data->arrangor as $arrangor)
-        @if($arrangor['has_link'])
-        <a href="{{ $arrangor['link_url'] }}" target="{{ $arrangor['link_target'] }}">{{ $arrangor['link_title'] }}</a><br>
-        @endif
-      @endforeach
-  @endforeach
-@endif
-
-```
-
-
-## Exempel på hur arrayen ser ut
-
-```sh
-array (size=2)
-  0 => 
-    object(WP_Post)[6833]
-      public 'ID' => int 51
-      public 'post_author' => string '1' (length=1)
-      public 'post_date' => string '2019-06-07 11:28:09' (length=19)
-      public 'post_date_gmt' => string '2019-06-07 09:28:09' (length=19)
-      public 'post_content' => string 'Min första spelning' (length=19)
-      public 'post_title' => string 'Första spelningen' (length=17)
-      public 'post_excerpt' => string '' (length=0)
-      public 'post_status' => string 'publish' (length=7)
-      public 'comment_status' => string 'closed' (length=6)
-      public 'ping_status' => string 'closed' (length=6)
-      public 'post_password' => string '' (length=0)
-      public 'post_name' => string 'forsta-spelningen' (length=3)
-      public 'to_ping' => string '' (length=0)
-      public 'pinged' => string '' (length=0)
-      public 'post_modified' => string '2019-06-07 11:32:40' (length=19)
-      public 'post_modified_gmt' => string '2019-06-07 09:32:40' (length=19)
-      public 'post_content_filtered' => string '' (length=0)
-      public 'post_parent' => int 0
-      public 'guid' => string 'http://exempel.se/evenemang/forsta-spelningen/' (length=46)
-      public 'menu_order' => int 0
-      public 'post_type' => string 'evenemang' (length=9)
-      public 'post_mime_type' => string '' (length=0)
-      public 'comment_count' => string '0' (length=1)
-      public 'filter' => string 'raw' (length=3)
-      public 'url' => string 'http://exempel.se/evenemang/forsta-spelningen' (length=45)
-      public 'image_url' => string 'http://exempel.se/app/uploads/2019/06/hero-d-lopning.jpg' (length=56)
-      public 'date' => string '2019-06-07' (length=10)
-      public 'stad' => string 'Halmstad' (length=8)
-      public 'spelstalle' => string 'Galgberget' (length=10)
-      public 'speltid' => string '2019-06-29 18:30' (length=16)
-      public 'speltid_datum' => string '2019-06-29' (length=10)
-      public 'speltid_tid' => string '18:30' (length=5)
-      public 'information' => 
-        array (size=3)
-            0 => 
-              array (size=4)
-                'has_link' => int 1
-                'link_title' => string 'Om oss' (length=6)
-                'link_url' => string 'http://exempel.se/om-oss/' (length=25)
-                'link_target' => string '' (length=0)
-            1 => 
-              array (size=4)
-                'has_link' => int 1
-                'link_title' => string 'För artister' (length=13)
-                'link_url' => string 'http://exempel.se/for-artister/' (length=31)
-                'link_target' => string '' (length=0)
-            2 => 
-              array (size=4)
-                'has_link' => int 1
-                'link_title' => string 'För arrangörer' (length=16)
-                'link_url' => string 'http://exempel.se/for-arrangorer/' (length=33)
-                'link_target' => string '' (length=0)
-      public 'arrangor' => 
-        array (size=1)
-          0 => 
-            array (size=4)
-              'has_link' => int 1
-              'link_title' => string 'Artister' (length=8)
-              'link_url' => string 'http://www.exempel.se' (length=21)
-              'link_target' => string '' (length=0)
-```
-
-
-## Vis aevenemangets ingress på en enskild evenemangs-sida
+## Visa evenemangets ingress på en enskild evenemangs-sida
 
 ```sh
 Evenemangets ingress<br>
@@ -210,7 +118,17 @@ Evenemangets ingress<br>
 ```
 
 
-## Visa evenemangets ingress på en enskild evenemangs-sida
+## Visa länk till biljett-köp
+
+```sh
+@php($myBiljett = get_region_halland_acf_page_evenemang_biljett())
+@if($myBiljett['biljett_has_link'] == 1)
+  <a href="{{ $myBiljett['biljett_url'] }}">{{ $myBiljett['biljett_title'] }}</a>
+@endif
+```
+
+
+## Visa evenemangs-data på en enskild evenemangs-sida
 
 ```sh
 <div>
@@ -238,6 +156,13 @@ Evenemangets ingress<br>
 
 
 ## Versionhistorik
+
+### 2.0.0
+- Funktion för att hämta ut all data borttagen
+- Lagt till bild-puff som acf-fält
+- Lagt till biljett-url som acf-fält
+- Puff-bilden hämtas ut när man hämtar kommande evenemang
+- Funktion för att hämta ut biljett på en enskild sida
 
 ### 1.3.0
 - Funktioner för att visa evenemangs-data på en enskild sida
--- region_halland_acf_page_evenemang.php
+++ region_halland_acf_page_evenemang.php
@@ -6,7 +6,7 @@
 	/*
 	Plugin Name: Region Halland ACF Page Evenemang
 	Description: ACF-fält för extra fält nederst på en evenemangs-sida
-	Version: 1.3.0
+	Version: 2.0.0
 	Author: Roland Hydén
 	License: MIT
 	Text Domain: regionhalland
@@ -130,6 +130,45 @@
 		              'first_day' => 1, 
 		           	),
 		        	4 => array(
+			        	'key' => 'field_1000163',
+					    'label' => 'Länk till biljettköp',
+					    'name' => 'name_1000164',
+					    'type' => 'link',
+					    'instructions' => '',
+					    'required' => 0,
+					    'conditional_logic' => 0,
+					    'wrapper' => [
+					        'width' => '',
+					        'class' => '',
+					        'id' => '',
+					    ],
+					    'return_format' => 'array',
+			        ),
+		        	5 => array(
+			        	'key' => 'field_1000165',
+					    'label' => 'Bild för puff (inte för sidan)',
+					    'name' => 'name_1000166',
+					    'type' => 'image',
+					    'instructions' => '',
+					    'required' => 0,
+					    'conditional_logic' => 0,
+					    'wrapper' => [
+					        'width' => '',
+					        'class' => '',
+					        'id' => '',
+					    ],
+					    'return_format' => 'array',
+					    'preview_size' => 'thumbnail',
+					    'library' => 'all',
+					    'min_width' => '',
+					    'min_height' => '',
+					    'min_size' => '',
+					    'max_width' => '',
+					    'max_height' => '',
+					    'max_size' => '',
+					    'mime_types' => '',
+			        ),
+					6 => array(
 			            'key' => 'field_1000155',
 			            'label' => __('Information', 'halland'),
 			            'name' => 'name_1000156',
@@ -165,7 +204,7 @@
 		        		  ),
 			            ),
 			        ),
-		        	5 => array(
+		        	7 => array(
 			            'key' => 'field_1000159',
 			            'label' => __('Arrangörer', 'halland'),
 			            'name' => 'name_1000160',
@@ -274,73 +313,31 @@
 			$page->speltid = get_field('name_1000154', $page->ID);
 			$page->speltid_datum = region_halland_acf_page_evenemang_get_datum($page->speltid);
 			$page->speltid_tid = region_halland_acf_page_evenemang_get_tid($page->speltid);
-		
-		}
-
-		// Returnera array med alla poster
-		return $myPages;
-
-	}
-
-	function get_region_halland_acf_page_evenemang_kommande_items_all($myAntal = -1) {
-		
-		$date = date("Y-m-d H:i:s");
-		
-		// Preparerar array för att hämta ut nyheter
-		$args = array( 
-			'post_type' => 'evenemang',
-			'posts_per_page' => $myAntal,
-			'meta_key' => 'name_1000154',
-            'orderby' => 'meta_value meta_value_num',
-            'order' => 'DESC',
-            'meta_query' => array(
-                'relation' => 'AND',
-                array(
-                    'key'       => 'name_1000154',
-                    'compare'   => '>=',
-                    'value'     => $date,
-                )
-            )
-		);
-
-		// Hämta valda nyheter
-		$myPages = get_posts($args);
-		
-		foreach ($myPages as $page) {
-
-			// Lägg till sidans url 	
-			$page->url = get_permalink($page->ID);
-
-			// Bild
-			$page->image = get_the_post_thumbnail($page->ID);
-			$page->image_url = get_the_post_thumbnail_url($page->ID);
 			
-			// Publicerad datum
-			$page->date = get_the_date('Y-m-d', $page->ID);
+			// Länk till biljett		
+			$link_field_object = get_field('field_1000163', $page->ID);
+			$page->biljett_title = $link_field_object['title'];
+			$page->biljett_link = $link_field_object['url'];
+			$page->biljett_target = $link_field_object['target'];
+			if ($page->biljett_link) {
+				$page->biljett_has_link = 1;
+			} else {
+				$page->biljett_has_link = 0;
+			}
 
-			// Ingress
-			$page->ingress = get_field('name_1000148', $page->ID);
+			// Puff-bild
+			$image_field_object = get_field('field_1000165', $page->ID);
+			$page->puff_url = $image_field_object['url'];
+			$page->puff_width = $image_field_object['width'];
+			$page->puff_height = $image_field_object['height'];
+			if ($page->puff_url) {
+				$page->puff_has_url = 1;
+			} else {
+				$page->puff_has_url = 0;
+			}
 
-			// Stad
-			$page->stad = get_field('name_1000150', $page->ID);
-
-			// Spelställe
-			$page->spelstalle = get_field('name_1000152', $page->ID);
-			
-			// Speltid
-			$page->speltid = get_field('name_1000154', $page->ID);
-			$page->speltid_datum = region_halland_acf_page_evenemang_get_datum($page->speltid);
-			$page->speltid_tid = region_halland_acf_page_evenemang_get_tid($page->speltid);
-
-			// Hämta ACF-objektet för link
-			$information_field_object = get_field('name_1000156', $page->ID);
-			$arrangor_field_object = get_field('name_1000160', $page->ID);
-
-			$page->information = region_halland_acf_page_evenemang_get_link_data($information_field_object, "name_1000158");			
-			$page->arrangor = region_halland_acf_page_evenemang_get_link_data($arrangor_field_object, "name_1000162");
-		
 		}
-
+		
 		// Returnera array med alla poster
 		return $myPages;
 
@@ -382,10 +379,34 @@
 		return $myData;			
 	}
 
-	function region_halland_acf_page_evenemang_get_link_data($page_field_object, $acf_name) {
-		
+	function get_region_halland_acf_page_evenemang_biljett() {
+		$field_object = get_field('field_1000163');
+		$myData = region_halland_acf_page_evenemang_get_biljett_data($field_object);
+		return $myData;			
+	}
+	
+	function region_halland_acf_page_evenemang_get_biljett_data($field_object) {
+
+		// Preparera array
 		$myData = array();
-        foreach ($page_field_object as $value) {
+		$myData['biljett_title'] = $field_object['title'];
+		$myData['biljett_url'] = $field_object['url'];
+		$myData['biljett_target'] = $field_object['target'];
+		if ($field_object['url']) {
+			$myData['biljett_has_link'] = 1;
+		} else {
+			$myData['biljett_has_link'] = 0;
+		}
+
+		// Returnera array
+		return $myData;
+	}
+
+	function region_halland_acf_page_evenemang_get_link_data($field_object, $acf_name) {
+		
+		// Preparera array
+		$myData = array();
+        foreach ($field_object as $value) {
 	        $arrLink = $value[$acf_name];
 	        if (is_array($arrLink)) {
 		        $intHasLink = 1;
@@ -406,6 +427,7 @@
 	        ));
         }
 
+		// Returnera array
 		return $myData;
 
 	}
